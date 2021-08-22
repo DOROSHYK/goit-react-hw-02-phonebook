e927e9f6b9206347353af3448ac6f82518969560
Doroshuk <dorosukolga100@gmail.com>
2021-08-22 23:20:14 +0300

    add some for start

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,31 @@
 import React, { Component } from 'react';
+import shortid from 'shortid';
 import InputForm from './component/InputForm/InputForm';
 
 class App extends Component {
   state = {
     contacts: [],
     name: '',
+    number: '',
+  };
+
+  addContacts = ({ name, number }) => {
+    const contact = {
+      name,
+      number,
+      id: shortid.generate(),
+    };
+
+    this.setState(({ contacts }) => ({
+      contacts: [contact, ...contacts],
+    }));
   };
 
   render() {
+    // const { contacts } = this.state;
     return (
       <>
-        <InputForm />
+        <InputForm onSubmit={this.addContacts} />
       </>
     );
   }
--- src/component/InputForm/InputForm.jsx
+++ src/component/InputForm/InputForm.jsx
@@ -7,31 +7,54 @@ class InputForm extends Component {
     number: '',
   };
 
+  handelChange = event => {
+    const { name, value } = event.currentTarget;
+    this.setState({ [name]: value });
+    console.log(event.currentTarget);
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    this.props.onSubmit(this.state);
+    this.reset();
+  };
+
+  reset = () => {
+    this.setState({ name: '', number: '' });
+  };
+
   render() {
+    const { name, number } = this.state;
     return (
-      <form>
+      <form onSubmit={this.handleSubmit}>
         <label>
           NAME
           <input
-            type="tel"
-            name="number"
-            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-            title="Номер телефона должен состоять цифр и может содержать пробелы, тире, круглые скобки и может начинаться с +"
-            required
+            id="name"
+            value={name}
+            onChange={this.handelChange}
+            type="tex"
+            name="name"
+            // pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+            // title="Номер телефона должен состоять цифр и может содержать пробелы, тире, круглые скобки и может начинаться с +"
+            // required
           />
         </label>
 
         <label>
-          {' '}
           TEL
           <input
-            type="tel"
+            id="number"
+            value={number}
+            onChange={this.handelChange}
+            type="tex"
             name="number"
-            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-            title="Номер телефона должен состоять цифр и может содержать пробелы, тире, круглые скобки и может начинаться с +"
-            required
+            // pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+            // title="Номер телефона должен состоять цифр и может содержать пробелы, тире, круглые скобки и может начинаться с +"
+            // required
           />
         </label>
+        <button type="submit">Add contact</button>
       </form>
     );
   }
